583dec2093dc52990c00db36dee9a8c946583278
ipcrm <mcadorette@puppetlabs.com>
2016-03-24 16:14:50 -0400

    test commit

--- manifests/site.pp
+++ manifests/site.pp
@@ -10,6 +10,7 @@
 # node definitions.)
 
 ## Active Configurations ##
+## TEST ##
 
 # Disable filebucket by default for all File resources:
 #http://docs.puppetlabs.com/pe/latest/release_notes.html#filebucket-resource-no-longer-created-by-default
